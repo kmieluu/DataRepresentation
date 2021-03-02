7868df5139d0533d954abfff268ff9ecc572604d
Agata Chmielowiec <a.k.chmmielowiec@gmail.com>
2021-03-02 18:22:20 +0000

    wrote sql including 3 databases to be put on server via SQL

---
+++ Big Project/FlaskServerWithRestApiDogs.py
@@ -0,0 +1,103 @@
+#!flask/bin/python
+from flask import Flask, jsonify,  request, abort, make_response
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='../')
+
+dogs = [
+    {
+        "Registration Number" : "-",
+        "breed":"Puddle",
+        "age":2,
+        "IKC Registered":"No",
+        "price":200
+    },
+    {
+        "Registration Number" : 1234,
+        "breed":"Vizsla",
+        "age":0.5,
+        "IKC Registered":"Yes",
+        "price":2000
+    },
+    {
+        "Registration Number" : "-",
+        "breed":"German Shepherd",
+        "age":"2 months",
+        "IKC Registered":"No",
+        "price":700
+    }
+]
+
+@app.route('/dogs', methods=['GET'])
+def get_dogs():
+    return jsonify( {'dogs':dogs})
+#curl -i http://localhost:5000/dogs
+
+
+@app.route('/dogs/<string:breed>', methods =['GET'])
+def get_dog(breed):
+    foundDogs = list(filter(lambda t : t['breed'] == breed , dogs))
+    if len(foundDogs) == 0:
+        return jsonify( { 'dog' : '' }),204
+    return jsonify( { 'dog' : foundDogs[0] })
+    #curl -i http://localhost:5000/dogs/test
+
+@app.route('/dogs', methods=['POST'])
+def create_dog():
+    if not request.json:
+        abort(400)
+    if not 'dog' in request.json:
+        abort(400)
+    dog={
+        "breed":  request.json['breed'],
+        "age": request.json['age'],
+        "IKC registeded":request.json['model'],
+        "price":request.json['price']
+        }
+    dogs.append(dog)
+    return jsonify( {'dog':dog }),201
+# sample test
+# curl -i -H "Content-Type:application/json" -X POST -d '{"reg":"12 D 1234","make":"Fiat","model":"Punto","price":3000}' http://localhost:5000/cars
+# for windows use this one
+# curl -i -H "Content-Type:application/json" -X POST -d "{\"IKC Registered\":\"Yes\",\"age\":\"12345\",\"breed\"chiuaua\",\"price\":3000}" http://localhost:5000/dogs
+@app.route('/dogs/<string:breed>', methods =['PUT'])
+def update_dog(breed):
+    foundDogs=list(filter(lambda t : t['breed'] ==breed, dogs))
+    if len(foundDogs) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'age' in request.json and type(request.json['age']) != str:
+        abort(400)
+    if 'IKC registered' in request.json and type(request.json['IKC registered']) is not str:
+        abort(400)
+    if 'price' in request.json and type(request.json['price']) is not int:
+        abort(400)
+    foundDogs[0]['age']  = request.json.get('age', foundDogs[0]['age'])
+    foundDogs[0]['IKC registered'] =request.json.get('IKC registered', foundDogs[0]['IKC registered'])
+    foundDogs[0]['price'] =request.json.get('price', foundDogs[0]['price'])
+    return jsonify( {'dog':foundDogs[0]})
+#curl -i -H "Content-Type:application/json" -X PUT -d '{"make":"Fiesta"}' http://localhost:5000/cars/181%20G%201234
+# for windows use this one
+#curl -i -H "Content-Type:application/json" -X PUT -d "{\"make\":\"Fiesta\"}" http://localhost:5000/cars/181%20G%201234
+
+@app.route('/dogs/<string:breed>', methods =['DELETE'])
+def delete_dog(breed):
+    foundDogs = list(filter (lambda t : t['breed'] == breed, dogs))
+    if len(foundDogs) == 0:
+        abort(404)
+    dogs.remove(foundDogs[0])
+    return  jsonify( { 'result':True })
+
+@app.errorhandler(404)
+def not_found404(error):
+    return make_response( jsonify( {'error':'Not found' }), 404)
+
+@app.errorhandler(400)
+def not_found400(error):
+    return make_response( jsonify( {'error':'Bad Request' }), 400)
+
+
+if __name__ == '__main__' :
+    app.run(debug= True)
---
+++ Big Project/bigprojectdatabases.sql
@@ -0,0 +1,89 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Table structure for table `dogs`
+--
+
+DROP TABLE IF EXISTS `dogs`;
+CREATE TABLE IF NOT EXISTS `dogs` (
+  `IKCReg` varchar(100) NOT NULL,
+  `RegNum` int(20) NOT NULL,
+  `age` int (10) NOT NULL,
+  `breed` varchar(100) NOT NULL,
+  'price' int(10) NOT NULL,
+  'id' int(10) NOT NULL
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `dogs`
+--
+
+INSERT INTO `dogs` (`IKCReg`, `RegNum`, `age`, `breed`, 'price', 'id') VALUES
+('Yes', 12000, 2, "pudle", 100, 1),
+('No', "", 1, 1, "chiuaua", 123, 2),
+('Yes', 1234, 4, "husky", 2000, 3),
+('No', "", 1, "german shepherd", 1000, 4),
+('Yes', 96336, 3, "doodle", 2123, 5),
+('No', "", 4, "berneese", 30, 6),
+('Yes', 654654, 1, "vizsla", 300, 7),
+('Yes', 987987, 2, "mix race",120, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `competitions`
+--
+
+DROP TABLE IF EXISTS `competitions`;
+CREATE TABLE IF NOT EXISTS `competitions` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descr` varchar(250) DEFAULT NULL,
+  `participants` int(5) DEFAULT NULL,
+  `prize` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `countries` (`countries`)
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `competitions`
+--
+
+INSERT INTO `competitions` (`id`, `descr`, `participants`, `prize`) VALUES
+(1, "Championship of Sydney", 200, "$ 1000"),
+(3, "Championship of Moscow", 40, "$ 10000"),
+(5, "Championship of Dublin", 60, "$ 10000");
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+CREATE TABLE IF NOT EXISTS `countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owners name` varchar(30) NOT NULL,
+  `Level of knowledge` varchar(50) NOT NULL,
+  
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `owners name` (`owners name`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `owners name`, `Level of knowledge`) VALUES
+(1, 'Alex Baldwin', 'Pro owner'),
+(2, 'Stanislaw Sojka', 'Beginner'),
+(3, 'Sarah Jessica Parker', 'Pro owner');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
